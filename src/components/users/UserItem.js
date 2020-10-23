08c5298c8287d06a87d4af07ccbec44deda0da68
Dylan Cooper <dylancooper155@gmail.com>
2020-10-22 21:47:37 -0400

    search function working. Added auth key

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -5,7 +5,7 @@ const UserItem = ({user: { login, avatar_url, html_url } }) => {
     
         return (
             <div className="card text-center">
-                <img src={avatar_url} alt="image" className="round-image" style={{ width: '60px' }}/>
+                <img src={avatar_url} alt="" className="round-image" style={{ width: '60px' }}/>
              <h3>{login}</h3>
              <div>
                  <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
